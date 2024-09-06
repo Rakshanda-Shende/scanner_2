8f34e7b83e9d9ae49435877fa3133d97ec0238c3
Rakshanda-Shende <141413725+Rakshanda-Shende@users.noreply.github.com>
2024-09-06 20:32:58 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7c5f99df3d5508a8e729830ae374c512",
+const RESOURCES = {"flutter_bootstrap.js": "8fbf94e0e6a055eae9cafab6926dfefb",
 "version.json": "e5a884528c1f3914ee2a4bc843406cd5",
-"index.html": "d9f41afa12c2fef34931eb41e0fff9fa",
-"/": "d9f41afa12c2fef34931eb41e0fff9fa",
+"index.html": "8de00e4a717d18e099bf1609e9fbea92",
+"/": "8de00e4a717d18e099bf1609e9fbea92",
 "js/barcode.js": "ff15fabb8d5f336bc97287cf37bb4915",
 "main.dart.js": "8ca91dc9f82247d8f301b48822611edb",
 "worker.js": "381cf01d5369367216f2bbdeaaa5ed37",
